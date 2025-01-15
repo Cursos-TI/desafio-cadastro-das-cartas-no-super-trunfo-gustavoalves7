599b68c4ff216662d133a8229c052cb05fbf6a44
gustavoalves7 <sanseverinoalvesgustavo@gmail.com>
2025-01-15 02:00:50 +0000

    criei o adicionar populacao e area

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -13,10 +13,26 @@ int main() {
     // Cadastro das Cartas:
     // Sugestão: Utilize a função scanf para capturar as entradas do usuário para cada atributo.
     // Solicite ao usuário que insira as informações de cada cidade, como o código, nome, população, área, etc.
+    int pop;
+    float area;
+    int pt;
+    printf("Digite a populacao: ");
+    scanf("%d", &pop);
+    printf("Sua populacao: %d\n", pop);
+
+    printf("Digite a area: ");
+    scanf("%f", &area);
+    printf("Sua populacao: %f\n", area);
     
+    printf("Digite quantos pontos turisticos existe: ");
+    scanf("%e", &pt);
+    printf("Seu ponto turistico: %e\n", pt);
+
+    printf("populacao:%d\n, area:%f\n, pontos turistico:%e");
+
+
     // Exibição dos Dados das Cartas:
     // Sugestão: Utilize a função printf para exibir as informações das cartas cadastradas de forma clara e organizada.
     // Exiba os valores inseridos para cada atributo da cidade, um por linha.
-
     return 0;
 }
